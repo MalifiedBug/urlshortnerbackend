5abcc2c7dcda42703a8b10b83db077caada729d0
omkar lolge <omkarlolge119.ol@gmail.com>
2022-12-03 13:55:17 +0530

    first commit

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
 import { MongoClient } from "mongodb";
-import cors from 'cors'
+import cors from "cors";
 // const express = require("express"); // "type": "commonjs"
 import express, { response } from "express"; // "type": "module"
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-import mongoose from 'mongoose';
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import mongoose from "mongoose";
 import { nanoid } from "nanoid";
 dotenv.config();
 
-
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 const PORT = process.env.PORT;
-const BASE = "http://localhost:4000"
-const MONGO_URL = process.env.MONGO_URL
+const BASE = "http://localhost:4000";
+const MONGO_URL = process.env.MONGO_URL;
 
-async function MongoConnect(){
+async function MongoConnect() {
   const client = await new MongoClient(MONGO_URL).connect();
-  console.log('Mongo Connected')
+  console.log("Mongo Connected");
   return client;
 }
 
 const client = await MongoConnect();
 
 app.use(
-    cors({
-      origin:'http://localhost:3000',
-      credentials: true,
-    })
-  );
-
-
-
-
-
-
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.get("/", function (request, response) {
   response.send("🙋‍♂️ URL shortener");
 });
 
+app.post("/short", async (req, res) => {
+  const found = await client
+    .db("UrlShortner")
+    .collection("urls")
+    .findOne({ full: req.body.full });
+  if (found) {
+    res.send(found);
+  } else {
+    const id = nanoid(4);
+    const short = `${id}`;
+    const insert = await client
+      .db("UrlShortner")
+      .collection("urls")
+      .insertOne({ full: req.body.full, short: short, count: 0 });
+    res.send(insert,short);
+  }
+});
 
-app.post('/short', async (req, res) => {
-    const found = await client.db("UrlShortner").collection("urls").findOne({full: req.body.full});
-    if(found){
-      res.send(found);
-    }
-    else{
-      const id = nanoid(4);
-      const short = `${id}`
-      const insert = await client.db("UrlShortner").collection("urls").insertOne({full: req.body.full,short:short,count:0});
-      res.send(insert);
-    }
-  });
-
-
-  app.get('/:shortUrl', async (req, res) => {
-   const short = req.params.shortUrl;
-   const found = await client.db("UrlShortner").collection("urls").findOne({short:short});
-   if(found){
+app.get("/:shortUrl", async (req, res) => {
+  const short = req.params.shortUrl;
+  const found = await client
+    .db("UrlShortner")
+    .collection("urls")
+    .findOne({ short: short });
+  if (found) {
     res.redirect(`${found.full}`);
-    await client.db("UrlShortner").collection("urls").updateOne({short:short},{$inc: { count: 1 }});
-   }else{res.status(404).send({msg:"not found"})}   
-  });
-
-
-
+    await client
+      .db("UrlShortner")
+      .collection("urls")
+      .updateOne({ short: short }, { $inc: { count: 1 } });
+  } else {
+    res.status(404).send({ msg: "not found" });
+  }
+});
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
-
